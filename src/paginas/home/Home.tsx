38876b4c582ea551d2ae28085cdcdf83f6475f94
unknown <lcapelotto@gmail.com>
2023-03-31 10:04:18 -0300

    meu-primeiro-componente

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 >Home</h1>
+           
+        </>
+    );
+}
+
+export default Home;
