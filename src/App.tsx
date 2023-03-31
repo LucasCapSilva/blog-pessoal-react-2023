53de1cdd1e0ed21f24a5766d5f09e4c560d1ad52
unknown <lcapelotto@gmail.com>
2023-03-31 12:36:52 -0300

    hooks-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
 import Home from './paginas/home/Home';
 import './App.css';
-
+import React, { useState } from 'react';
 
 function App() {
+  const [valor, setValor] = useState(0);
+
+  function handleClick() {
+    setValor(valor + 1);
+  }
   return (
-   <Home  title="Componente Home"
-          description="Este é um componente Home que recebe props."/>
+    //  <Home  title="Componente Home"
+    //         description="Este é um componente Home que recebe props."/>
+    <div>
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+    </div>
   );
 }
 
