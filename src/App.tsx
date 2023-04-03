fb0701557d0d232bf5bfaa7e079efa7db21ef6c8
unknown <lcapelotto@gmail.com>
2023-04-03 09:34:19 -0300

    flex-box

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './paginas/home/Home';
 
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      Hello world!
-    </h1>
+    <Home/>
     </>
 );
 }
