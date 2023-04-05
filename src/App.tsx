bc1af1c14a4f4d5c09997d6242d1244d7f5ef2b8
unknown <lcapelotto@gmail.com>
2023-04-05 09:39:32 -0300

    cadastro-template

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
     <>
-      <AuthProvider>
+   
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Login />} />
+              <Route path="/" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
@@ -25,7 +25,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-      </AuthProvider>
+  
     </>
   );
 }
