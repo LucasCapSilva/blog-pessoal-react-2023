a19cb5112c4326c6d8aadac1ee78ebd42ce97889
unknown <lcapelotto@gmail.com>
2023-04-10 16:25:25 -0300

    navbar-footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 
