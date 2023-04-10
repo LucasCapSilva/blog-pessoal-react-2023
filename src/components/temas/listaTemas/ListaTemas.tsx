9ae296167970a0f5df59666a23c5b49b055ecdb2
unknown <lcapelotto@gmail.com>
2023-04-10 09:01:58 -0300

    getAll

--- src/components/temas/listaTemas/ListaTemas.tsx
+++ src/components/temas/listaTemas/ListaTemas.tsx
@@ -1,17 +1,55 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { Dna } from 'react-loader-spinner';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthContext';
+import Tema from '../../../models/Tema';
+import { buscar } from '../../../services/Service';
 import CardTemas from '../cardTemas/CardTemas';
 
 function ListaTemas() {
-  
+  const [temas, setTemas] = useState<Tema[]>([]);
+
+  let navigate = useNavigate();
+
+  const { usuario } = useContext(AuthContext);
+  const token = usuario.token;
+
+  async function buscarTemas() {
+    await buscar('/temas', setTemas, {
+      headers: { Authorization: token },
+    });
+  }
+
+  useEffect(() => {
+    if (token === '') {
+      alert('Você precisa estar logado');
+      navigate('/login');
+    }
+  }, [token]);
+
+  useEffect(() => {
+    buscarTemas();
+  }, [temas.length]);
   return (
     <>
-    
+      {temas.length === 0 && (
+        <Dna
+          visible={true}
+          height="200"
+          width="200"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper mx-auto"
+        />
+      )}
       <div className="flex justify-center w-full my-4">
         <div className="container flex flex-col">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {temas.map((tema) => (
               <>
-                <CardTemas  />
+                <CardTemas key={tema.id} tema={tema} />
               </>
+            ))}
           </div>
         </div>
       </div>
