73f86c3d6edaa5d4e5b1d9b63ad6402d69c1ff00
unknown <lcapelotto@gmail.com>
2023-04-10 10:02:54 -0300

    cadastrar-tema-logica

--- src/components/temas/formularioTema/FormularioTema.tsx
+++ src/components/temas/formularioTema/FormularioTema.tsx
@@ -1,36 +1,116 @@
-import React from 'react';
-
-
+import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthContext';
+import Tema from '../../../models/Tema';
+import { atualizar, buscar, cadastrar } from '../../../services/Service';
 
 function FormularioTema() {
+  const [tema, setTema] = useState<Tema>({} as Tema);
 
+  let navigate = useNavigate();
 
-    return (
-        <div className="container flex flex-col items-center justify-center mx-auto">
-            <h1 className="text-4xl text-center my-8">
-                Cadastrar tema
-            </h1>
+  const { id } = useParams<{ id: string }>();
 
-            <form className="w-1/2 flex flex-col gap-4" >
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="descricao">Descrição do tema</label>
-                    <input
-                        type="text"
-                        placeholder="Descrição"
-                        name='descricao'
-                        className="border-2 border-slate-700 rounded p-2"
+  const { usuario } = useContext(AuthContext);
+  const token = usuario.token;
 
-                    />
-                </div>
-                <button
-                    className="rounded text-slate-100 bg-indigo-400 hover:bg-indigo-800 w-1/2 py-2 mx-auto block"
-                    type="submit"
-                >
-                    Cadastrar
-                </button>
-            </form>
+  async function buscarPorId(id: string) {
+    await buscar(`/temas/${id}`, setTema, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  useEffect(() => {
+    if (id !== undefined) {
+        buscarPorId(id)
+    }
+}, [id])
+
+function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+  setTema({
+      ...tema,
+      [e.target.name]: e.target.value
+  })
+
+  console.log(JSON.stringify(tema))
+}
+
+async function gerarNovoTema(e: ChangeEvent<HTMLFormElement>) {
+  e.preventDefault()
+
+  if (id !== undefined) {
+      try {
+          await atualizar(`/temas`, tema, setTema, {
+              headers: {
+                  'Authorization': token
+              }
+          })
+
+          alert('Tema atualizado com sucesso')
+          retornar()
+
+      } catch (error) {
+          alert('Erro ao atualizar o Tema')
+      }
+
+  } else {
+      try {
+          await cadastrar(`/temas`, tema, setTema, {
+              headers: {
+                  'Authorization': token
+              }
+          })
+
+          alert('Tema cadastrado com sucesso')
+
+      } catch (error) {
+          alert('Erro ao cadastrado o Tema')
+      }
+  }
+
+  retornar()
+}
+
+function retornar() {
+  navigate("/temas")
+}
+
+  useEffect(() => {
+    if (token === '') {
+      alert('Você precisa estar logado');
+      navigate('/login');
+    }
+  }, [token]);
+
+  return (
+    <div className="container flex flex-col items-center justify-center mx-auto">
+      <h1 className="text-4xl text-center my-8">
+        {id === undefined ? 'Cadastre um novo tema' : 'Editar tema'}
+      </h1>
+
+      <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovoTema}>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="descricao">Descrição do tema</label>
+          <input
+            type="text"
+            placeholder="Descrição"
+            name='descricao'
+            className="border-2 border-slate-700 rounded p-2"
+            value={tema.descricao}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+          />
         </div>
-    );
+        <button
+          className="rounded text-slate-100 bg-indigo-400 hover:bg-indigo-800 w-1/2 py-2 mx-auto block"
+          type="submit"
+        >
+          {id === undefined ? 'Cadastrar' : 'Editar'}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default FormularioTema;
