7dadfcf5e6350518326de882695f49337e013151
unknown <lcapelotto@gmail.com>
2023-04-10 16:22:46 -0300

    1.0

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
+import homeLogo from '../../assets/home.png'
 import './Home.css';
+
 
 function Home() {
     return (
         <>
-            <div className="h-96 mb-96">
-
-                <div className="bg-indigo-900 flex justify-center">
-                    <div className='container grid grid-cols-2 text-white'>
-                        <div className="flex flex-col gap-4 items-center justify-center py-4">
-                            <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-                            <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-
-                            <div className="flex justify-around gap-4">
-
-                                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-                            </div>
-                        </div>
-
-                        <div className="flex justify-center ">
-                            <img src="https://i.imgur.com/fyfri1v.png" alt="" className='w-2/3' />
-                        </div>
-                    </div>
-                </div>
-
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+  
+              <div className="flex justify-around gap-4">
+              
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              </div>
             </div>
-        </>
+  
+            <div className="flex justify-center ">
+              <img src={homeLogo} alt="" className='w-2/3' />
+      
+            </div>
+          </div>
+        </div>
+      
+      </>
     );
 }
 
