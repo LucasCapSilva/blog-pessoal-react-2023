60e5010103db50ad18e263ea02567f9beb4ee92d
unknown <lcapelotto@gmail.com>
2023-04-11 09:26:49 -0300

    cadastrar-postagem-logica

--- src/components/postagens/formularioPostagem/FormularioPostagem.tsx
+++ src/components/postagens/formularioPostagem/FormularioPostagem.tsx
@@ -1,20 +1,140 @@
-import React from 'react';
-
+import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthContext';
+import Postagem from '../../../models/Postagem';
+import Tema from '../../../models/Tema';
+import { buscar, atualizar, cadastrar } from '../../../services/Service';
 
 
 function FormularioPostagem() {
-  
+  let navigate = useNavigate();
 
- 
+  const { id } = useParams<{ id: string }>();
+
+  const { usuario } = useContext(AuthContext);
+  const token = usuario.token;
+
+  const [temas, setTemas] = useState<Tema[]>([]);
+
+  const [tema, setTema] = useState<Tema>({
+    id: 0,
+    descricao: '',
+  });
+
+  const [postagem, setPostagem] = useState<Postagem>({
+    id: 0,
+    titulo: '',
+    texto: '',
+    data: '',
+    tema: null,
+    usuario: null,
+  });
+
+  async function buscarPostagemPorId(id: string) {
+    await buscar(`/postagens/${id}`, setPostagem, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  async function buscarTemaPorId(id: string) {
+    await buscar(`/temas/${id}`, setTema, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  async function buscarTemas() {
+    await buscar('/temas', setTemas, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  useEffect(() => {
+    if (token === '') {
+      alert('Você precisa estar logado');
+      navigate('/');
+    }
+  }, [token]);
+
+  useEffect(() => {
+    buscarTemas();
+    if (id !== undefined) {
+      buscarPostagemPorId(id);
+      console.log(tema);
+      
+    }
+  }, [id]);
+
+  useEffect(() => {
+    setPostagem({
+      ...postagem,
+      tema: tema,
+    });
+  }, [tema]);
+
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setPostagem({
+      ...postagem,
+      [e.target.name]: e.target.value,
+      tema: tema,
+      usuario: usuario,
+    });
+  }
+
+  function retornar() {
+    navigate('/postagens');
+  }
+
+  async function gerarNovaPostagem(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    console.log({postagem});
+
+    if (id != undefined) {
+      try {
+        await atualizar(`/postagens`, postagem, setPostagem, {
+          headers: {
+            Authorization: token,
+          },
+        });
+
+        alert('Postagem atualizada com sucesso');
+        retornar();
+      } catch (error) {
+        alert('Erro ao atualizar a Postagem');
+      }
+    } else {
+      try {
+        await cadastrar(`/postagens`, postagem, setPostagem, {
+          headers: {
+            Authorization: token,
+          },
+        });
+
+        alert('Postagem cadastrada com sucesso');
+        retornar();
+      } catch (error) {
+        alert('Erro ao cadastrar a Postagem');
+      }
+    }
+  }
+
+  const carregandoTema = tema.descricao === '';
   return (
     <div className="container flex flex-col mx-auto items-center">
-      <h1 className="text-4xl text-center my-8"></h1>
+      <h1 className="text-4xl text-center my-8">{id !== undefined ? 'Editar Postagem' : 'Cadastrar Postagem'}</h1>
 
-      <form  className="flex flex-col w-1/2 gap-4">
+      <form onSubmit={gerarNovaPostagem} className="flex flex-col w-1/2 gap-4">
         <div className="flex flex-col gap-2">
           <label htmlFor="titulo">Titulo da postagem</label>
           <input
-            
+            value={postagem.titulo}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             type="text"
             placeholder="Titulo"
             name="titulo"
@@ -25,7 +145,8 @@ function FormularioPostagem() {
         <div className="flex flex-col gap-2">
           <label htmlFor="titulo">Texto da postagem</label>
           <input
-            
+            value={postagem.texto}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             type="text"
             placeholder="Texto"
             name="texto"
@@ -35,17 +156,17 @@ function FormularioPostagem() {
         </div>
         <div className="flex flex-col gap-2">
           <p>Tema da postagem</p>
-          <select name="tema" id="tema" className='border p-2 border-slate-800 rounded' >
+          <select name="tema" id="tema" className='border p-2 border-slate-800 rounded' onChange={(e) => buscarTemaPorId(e.currentTarget.value)}>
             <option value="" selected disabled>Selecione um tema</option>
-           
+            {temas.map((tema) => (
               <>
-                <option >tema1</option>
+                <option value={tema.id} >{tema.descricao}</option>
               </>
-           
+            ))}
           </select>
         </div>
-        <button  type='submit' className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto block py-2'>
-         Cadastrar
+        <button disabled={carregandoTema} type='submit' className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto block py-2'>
+          {carregandoTema ? <span>Carregando</span> : id !== undefined ? 'Editar' : 'Cadastrar'}
         </button>
       </form>
     </div>
