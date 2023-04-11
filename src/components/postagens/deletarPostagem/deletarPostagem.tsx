bb84dad66cc757bb74a5cf0a606bbf40549a345e
unknown <lcapelotto@gmail.com>
2023-04-11 09:43:25 -0300

    deletar-postagem-logica

--- src/components/postagens/deletarPostagem/deletarPostagem.tsx
+++ src/components/postagens/deletarPostagem/deletarPostagem.tsx
@@ -1,8 +1,60 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AuthContext } from '../../../contexts/AuthContext'
+import Postagem from '../../../models/Postagem'
+import { buscar, deletar } from '../../../services/Service'
 
 function DeletarPostagem() {
- 
+  const [postagem, setPostagem] = useState<Postagem>({} as Postagem)
+
+    let navigate = useNavigate()
+
+    const { id } = useParams<{ id: string }>()
+
+    const { usuario } = useContext(AuthContext)
+    const token = usuario.token
+
+    async function buscarPorId(id: string) {
+        await buscar(`/postagens/${id}`, setPostagem, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado')
+            navigate('/login')
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== undefined) {
+            buscarPorId(id)
+        }
+    }, [id])
+
+    function retornar() {
+        navigate("/postagens")
+    }
+
+    async function deletarPostagem() {
+        try {
+            await deletar(`/postagens/${id}`, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+
+            alert('Postagem apagada com sucesso')
+
+        } catch (error) {
+            alert('Erro ao apagar a Postagem')
+        }
+
+        retornar()
+    }
   return (
     <div className='container w-1/3 mx-auto'>
       <h1 className='text-4xl text-center my-4'>Deletar postagem</h1>
@@ -12,12 +64,12 @@ function DeletarPostagem() {
       <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
       <header className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>Postagem</header>
       <div className="p-4">
-        <p className='text-xl h-full'>Titulo da postagem</p>
-        <p>texto da postagem</p>
+        <p className='text-xl h-full'>{postagem.titulo}</p>
+        <p>{postagem.texto}</p>
       </div>
       <div className="flex">
-        <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' >Não</button>
-        <button className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center' >
+        <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
+        <button className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center' onClick={deletarPostagem}>
           Sim
         </button>
       </div>
