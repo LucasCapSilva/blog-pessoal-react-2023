ce8e978e66d0580b04f5d28ab5474df23335fe60
unknown <lcapelotto@gmail.com>
2023-05-19 09:45:01 -0300

    1.0

--- src/components/postagens/deletarPostagem/deletarPostagem.tsx
+++ src/components/postagens/deletarPostagem/deletarPostagem.tsx
@@ -7,54 +7,61 @@ import { buscar, deletar } from '../../../services/Service'
 function DeletarPostagem() {
   const [postagem, setPostagem] = useState<Postagem>({} as Postagem)
 
-    let navigate = useNavigate()
+  let navigate = useNavigate()
 
-    const { id } = useParams<{ id: string }>()
+  const { id } = useParams<{ id: string }>()
 
-    const { usuario } = useContext(AuthContext)
-    const token = usuario.token
+  const { usuario, handleLogout } = useContext(AuthContext)
+  const token = usuario.token
 
-    async function buscarPorId(id: string) {
-        await buscar(`/postagens/${id}`, setPostagem, {
-            headers: {
-                'Authorization': token
-            }
-        })
+  async function buscarPorId(id: string) {
+    try {
+      await buscar(`/postagens/${id}`, setPostagem, {
+        headers: {
+          'Authorization': token
+        }
+      })
+    } catch (error: any) {
+      if (error.toString().includes('403')) {
+        alert('O token expirou, favor logar novamente')
+        handleLogout()
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (token === '') {
+      alert('Você precisa estar logado')
+      navigate('/login')
+    }
+  }, [token])
+
+  useEffect(() => {
+    if (id !== undefined) {
+      buscarPorId(id)
+    }
+  }, [id])
+
+  function retornar() {
+    navigate("/postagens")
+  }
+
+  async function deletarPostagem() {
+    try {
+      await deletar(`/postagens/${id}`, {
+        headers: {
+          'Authorization': token
+        }
+      })
+
+      alert('Postagem apagada com sucesso')
+
+    } catch (error) {
+      alert('Erro ao apagar a Postagem')
     }
 
-    useEffect(() => {
-        if (token === '') {
-            alert('Você precisa estar logado')
-            navigate('/login')
-        }
-    }, [token])
-
-    useEffect(() => {
-        if (id !== undefined) {
-            buscarPorId(id)
-        }
-    }, [id])
-
-    function retornar() {
-        navigate("/postagens")
-    }
-
-    async function deletarPostagem() {
-        try {
-            await deletar(`/postagens/${id}`, {
-                headers: {
-                    'Authorization': token
-                }
-            })
-
-            alert('Postagem apagada com sucesso')
-
-        } catch (error) {
-            alert('Erro ao apagar a Postagem')
-        }
-
-        retornar()
-    }
+    retornar()
+  }
   return (
     <div className='container w-1/3 mx-auto'>
       <h1 className='text-4xl text-center my-4'>Deletar postagem</h1>
@@ -62,18 +69,18 @@ function DeletarPostagem() {
       <p className='text-center font-semibold mb-4'>Você tem certeza de que deseja apagar a postagem a seguir?</p>
 
       <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-      <header className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>Postagem</header>
-      <div className="p-4">
-        <p className='text-xl h-full'>{postagem.titulo}</p>
-        <p>{postagem.texto}</p>
+        <header className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>Postagem</header>
+        <div className="p-4">
+          <p className='text-xl h-full'>{postagem.titulo}</p>
+          <p>{postagem.texto}</p>
+        </div>
+        <div className="flex">
+          <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
+          <button className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center' onClick={deletarPostagem}>
+            Sim
+          </button>
+        </div>
       </div>
-      <div className="flex">
-        <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
-        <button className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center' onClick={deletarPostagem}>
-          Sim
-        </button>
-      </div>
-    </div>
     </div>
   )
 }
