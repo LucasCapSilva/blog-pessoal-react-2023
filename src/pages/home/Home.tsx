9ec7fa28294299dfd4d938beef38418d8a382484
Jorge Felipe <silva.felipe12@hotmail.com>
2024-02-27 11:41:04 -0300

    Remove espaços  descessários para o codigo Home.tsx
    Removendo espaços entre os imports e a função e entre divs para melhor formatação do código

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,8 +4,6 @@ import './Home.css';
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
-
-
 function Home() {
     return (
         <>
@@ -14,16 +12,13 @@ function Home() {
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-  
               <div className="flex justify-around gap-4">
               <ModalPostagem />
               <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
             </div>
             </div>
-  
-            <div className="flex justify-center ">
+             <div className="flex justify-center ">
               <img src={homeLogo} alt="" className='w-2/3' />
-      
             </div>
           </div>
         </div>
